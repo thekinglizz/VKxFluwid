476be228e087901fdde713359c890c81f3f048bd
andrey.p <andrey.p@itfox-web.com>
2025-07-08 19:29:59 +0300

    minor fix

--- lib/view_models/scheme_view_model.dart
+++ lib/view_models/scheme_view_model.dart
@@ -1,5 +1,6 @@
 import 'dart:async';
 
+import 'package:flapp_widget/services/post_message_service/post_message_service.dart';
 import 'package:flapp_widget/view_models/user_view_model.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
@@ -12,24 +13,27 @@ import '../services/api_client.dart';
 import '../ui/widgets/scheme_widgets/seat_widget.dart';
 import 'action_view_model.dart';
 
-final asyncSchemeProvider = AsyncNotifierProvider<SVMState, SchemeViewModel>(() {
+final asyncSchemeProvider =
+    AsyncNotifierProvider<SVMState, SchemeViewModel>(() {
   return SVMState();
 });
 
-class SchemeRepository{
-  Future<SchemeViewModel> getSchemeViewModel(ActionViewModel avm) async{
-    final AssignedSeatsProcessor schemeData = await ActionAPI.computeSchemeData(avm
-        .selectedActionEvent!.actionEventId, avm.selectedActionEvent!.placementUrl!);
+class SchemeRepository {
+  Future<SchemeViewModel> getSchemeViewModel(ActionViewModel avm) async {
+    final AssignedSeatsProcessor schemeData = await ActionAPI.computeSchemeData(
+        avm.selectedActionEvent!.actionEventId,
+        avm.selectedActionEvent!.placementUrl!);
     schemeData.getSectorScaffoldList();
     //определяем уже выбранные места пользователя
     List<Seat> sList = [];
     totalSum.value = 0;
     for (var sector in schemeData.sectorList) {
-      for (var s in schemeData.siData.userSeatIdList){
-        if (sector.seatList.any((seat)=>seat.bil24seatObj.seatId == s)){
-          Seat tempSeat = sector.seatList.firstWhere((seat)=>seat.bil24seatObj.seatId == s);
+      for (var s in schemeData.siData.userSeatIdList) {
+        if (sector.seatList.any((seat) => seat.bil24seatObj.seatId == s)) {
+          Seat tempSeat = sector.seatList
+              .firstWhere((seat) => seat.bil24seatObj.seatId == s);
           sList.add(tempSeat);
-          if (tempSeat.category.tariffIdMap.isEmpty){
+          if (tempSeat.category.tariffIdMap.isEmpty) {
             totalSum.value += tempSeat.category.price;
           }
         }
@@ -53,10 +57,11 @@ class SchemeViewModel {
   final List<Seat> selectedSeats;
   final CategoryPriceObj? categoryPriceFilter;
   final Map<Seat, int> selectedTariffSeats;
-  SchemeViewModel({required this.schemeData,
-    required this.selectedSeats,
-    this.categoryPriceFilter,
-    required this.selectedTariffSeats});
+  SchemeViewModel(
+      {required this.schemeData,
+      required this.selectedSeats,
+      this.categoryPriceFilter,
+      required this.selectedTariffSeats});
 
   @override
   String toString() {
@@ -79,100 +84,117 @@ class SchemeViewModel {
   }
 }
 
-class SVMState extends AsyncNotifier<SchemeViewModel>{
+class SVMState extends AsyncNotifier<SchemeViewModel> {
   @override
   Future<SchemeViewModel> build() {
     final action = ref.watch(asyncActionProvider).value;
     return SchemeRepository().getSchemeViewModel(action as ActionViewModel);
   }
 
-  Future<dynamic> reserveSeat(Seat seat, User user, BuildContext context, String? fanId) async{
+  Future<dynamic> reserveSeat(
+      Seat seat, User user, BuildContext context, String? fanId) async {
     List<Seat> userSeatList = state.value!.selectedSeats;
-    dynamic reservationResult = await CartAPI.reserveSeats('RESERVE', user,
-        seat.bil24seatObj.seatId, null, null, fanId);
+    dynamic reservationResult = await CartAPI.reserveSeats(
+        'RESERVE', user, seat.bil24seatObj.seatId, null, null, fanId);
 
-    if (reservationResult is int){
+    if (reservationResult is int) {
       userSeatList.add(seat);
       ref.read(asyncUserProvider.notifier).incrementTotalSeatsInReserve();
       totalSum.value += seat.category.price;
-      state = AsyncValue.data(state.value!.copyWith(selectedSeats: userSeatList,
+      state = AsyncValue.data(state.value!.copyWith(
+          selectedSeats: userSeatList,
           categoryPriceFilter: state.value!.categoryPriceFilter));
     }
 
-    if (reservationResult is String){
-      if (context.mounted){
+    if (reservationResult is String) {
+      if (context.mounted) {
         await showDialog<String>(
             context: context,
             builder: (BuildContext context) {
-              return UserDialog(userMessage: reservationResult.toString(),
+              return UserDialog(
+                  userMessage: reservationResult.toString(),
                   redirectFlag: false);
             });
       }
     }
   }
 
-  Future<dynamic>  reserveSeatWithTariff(Seat seat, User user, BuildContext context,
-      int tariffId, double expectedPrice, String? fanId) async{
+  Future<dynamic> reserveSeatWithTariff(
+      Seat seat,
+      User user,
+      BuildContext context,
+      int tariffId,
+      double expectedPrice,
+      String? fanId) async {
     List<Seat> userSeatList = state.value!.selectedSeats;
     Map<Seat, int> tariffMap = state.value!.selectedTariffSeats;
 
-    if(tariffMap.containsKey(seat)){
+    if (tariffMap.containsKey(seat)) {
       totalSum.value -= seat.category.tariffIdMap[tariffMap[seat]]!;
       userSeatList.remove(seat);
       ref.read(asyncUserProvider.notifier).decrementTotalSeatsInReserve();
-      await CartAPI.reserveSeats('UN_RESERVE', user, seat.bil24seatObj.seatId, null, null, null);
+      await CartAPI.reserveSeats(
+          'UN_RESERVE', user, seat.bil24seatObj.seatId, null, null, null);
     }
 
     tariffMap[seat] = tariffId;
     dynamic reservationResult = await CartAPI.reserveSeats('RESERVE', user,
         seat.bil24seatObj.seatId, tariffId, expectedPrice, fanId);
 
-    if (reservationResult is int){
+    if (reservationResult is int) {
       userSeatList.add(seat);
       totalSum.value += expectedPrice;
       ref.read(asyncUserProvider.notifier).incrementTotalSeatsInReserve();
-      state = AsyncValue.data(state.value!.copyWith(selectedSeats: userSeatList,
-        selectedTariffSeats: tariffMap, categoryPriceFilter: state.value!.categoryPriceFilter ));
+      state = AsyncValue.data(state.value!.copyWith(
+          selectedSeats: userSeatList,
+          selectedTariffSeats: tariffMap,
+          categoryPriceFilter: state.value!.categoryPriceFilter));
     }
 
-    if (reservationResult is String){
-      if (context.mounted){
+    if (reservationResult is String) {
+      if (context.mounted) {
         await showDialog<String>(
             context: context,
             builder: (BuildContext context) {
-              return UserDialog(userMessage: reservationResult.toString(),
+              return UserDialog(
+                  userMessage: reservationResult.toString(),
                   redirectFlag: false);
             });
       }
     }
   }
 
-  dynamic unreserveSeat(Seat seat, User user, double? expectedPrice) async{
+  dynamic unreserveSeat(Seat seat, User user, double? expectedPrice) async {
     ref.read(asyncUserProvider.notifier).decrementTotalSeatsInReserve();
-    if (expectedPrice is double){
+    if (expectedPrice is double) {
       totalSum.value -= expectedPrice;
     } else {
       totalSum.value -= seat.category.price;
     }
     unselectSeatOnScheme(seat.bil24seatObj.seatId);
-    await CartAPI.reserveSeats('UN_RESERVE', user, seat.bil24seatObj.seatId, null, null, null);
+    await CartAPI.reserveSeats(
+        'UN_RESERVE', user, seat.bil24seatObj.seatId, null, null, null);
   }
 
-  void unselectSeatOnScheme(int seatId){
+  void unselectSeatOnScheme(int seatId) {
     List<Seat> userSeatList = state.value!.selectedSeats;
     Map<Seat, int> tempMap = state.value!.selectedTariffSeats;
-    userSeatList.removeWhere((element) => element.bil24seatObj.seatId == seatId);
-    tempMap.removeWhere((key, value)=> key.bil24seatObj.seatId == seatId);
-    state = AsyncValue.data(state.value!.copyWith(selectedSeats: userSeatList,
-        selectedTariffSeats: tempMap, categoryPriceFilter: state.value!.categoryPriceFilter));
+    userSeatList
+        .removeWhere((element) => element.bil24seatObj.seatId == seatId);
+    tempMap.removeWhere((key, value) => key.bil24seatObj.seatId == seatId);
+    state = AsyncValue.data(state.value!.copyWith(
+        selectedSeats: userSeatList,
+        selectedTariffSeats: tempMap,
+        categoryPriceFilter: state.value!.categoryPriceFilter));
   }
 
-  void clearSelectedSeats(){
-    state = AsyncValue.data(state.value!.copyWith(selectedSeats: [],
-        selectedTariffSeats: {}));
+  void clearSelectedSeats() {
+    state = AsyncValue.data(
+        state.value!.copyWith(selectedSeats: [], selectedTariffSeats: {}));
   }
 
-  void filterCategories(CategoryPriceObj? category){
-    state = AsyncValue.data(state.value!.copyWith(categoryPriceFilter: category));
+  void filterCategories(CategoryPriceObj? category) {
+    state =
+        AsyncValue.data(state.value!.copyWith(categoryPriceFilter: category));
   }
 }
